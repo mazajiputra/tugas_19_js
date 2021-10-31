9677e8dd4b8e1a595534b5ea139b0895eeef4aab
mazajiputra <mazajiputra@gmail.com>
2021-10-31 18:29:21 +0700

    Kumpul Tugas

---
+++ tugas19.js
@@ -0,0 +1,20 @@
+function tugas1() {
+console.log("Semangat...".repeat(5));
+    
+}
+
+function tugas2() {
+console.log("*****".repeat(1));
+console.log("*****".repeat(5));
+console.log("*****".repeat(9));
+console.log("*****".repeat(13));
+console.log("*****".repeat(19));
+console.log("*****".repeat(13));
+console.log("*****".repeat(9));
+console.log("*****".repeat(5));
+console.log("*****".repeat(1));
+    
+}
+
+tugas1()
+tugas2()
